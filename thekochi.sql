2456e85383968c5b82270416a09469a44d3f0925
Mazahir B Haroon <mazahirharoon@gmail.com>
2017-07-25 19:45:07 +0530

    updated the db

--- thekochi.sql
+++ thekochi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 25, 2017 at 01:08 PM
+-- Generation Time: Jul 25, 2017 at 02:13 PM
 -- Server version: 5.7.14
 -- PHP Version: 7.0.10
 
